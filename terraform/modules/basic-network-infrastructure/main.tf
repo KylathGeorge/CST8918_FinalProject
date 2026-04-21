967692aee62908fd0e293fd020f6388bab8b724c
Kylath George <kyla0001@algonquinlive.com>
2026-04-20 22:10:42 -0400

    Fix resource group name references in virtual network and subnet definitions

--- terraform/modules/basic-network-infrastructure/main.tf
+++ terraform/modules/basic-network-infrastructure/main.tf
@@ -5,36 +5,36 @@ data "azurerm_resource_group" "network" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-group-5"
   address_space       = ["10.0.0.0/14"]
-  location            = azurerm_resource_group.network.location
-  resource_group_name = azurerm_resource_group.network.name
+  location            = data.azurerm_resource_group.network.location
+  resource_group_name = data.azurerm_resource_group.network.name
   tags                = var.tags
 }
 
 # Subnets with Environment-based Octets
 resource "azurerm_subnet" "prod" {
   name                 = "prod"
-  resource_group_name  = azurerm_resource_group.network.name
+  resource_group_name  = data.azurerm_resource_group.network.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "test" {
   name                 = "test"
-  resource_group_name  = azurerm_resource_group.network.name
+  resource_group_name  = data.azurerm_resource_group.network.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "dev" {
   name                 = "dev"
-  resource_group_name  = azurerm_resource_group.network.name
+  resource_group_name  = data.azurerm_resource_group.network.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.2.0.0/16"]
 }
 
 resource "azurerm_subnet" "admin" {
   name                 = "admin"
-  resource_group_name  = azurerm_resource_group.network.name
+  resource_group_name  = data.azurerm_resource_group.network.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.3.0.0/16"]
 }
